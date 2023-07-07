7828154974dbbbde760fa320fec682223c1deba8
La-Haine-0 <ghonchar00@mail.ru>
2023-07-07 13:43:07 +0300

    fix file ddl-init.sql

--- ddl-init.sql
+++ ddl-init.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS books (
     book_name VARCHAR(255) NOT NULL,
     year_of_publishing_book DATE NOT NULL,
     book_size INTEGER NOT NULL,
-    book_priсe REAL NOT NULL
-    сount_copies_of_book INTEGER NOT NULL,
+    book_priсe REAL NOT NULL,
+    сount_copies_of_book INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS authors_books (
     authors_books_id uuid PRIMARY KEY,
@@ -25,13 +25,13 @@ CREATE TABLE IF NOT EXISTS publishing_houses_cities (
     publishing_houses_cities_id uuid PRIMARY KEY,
     publishing_houses_id,
     city_id uuid,
-    FOREIGN KEY (city_id) REFERENCES cities (city_id) ON DELETE SET NULL
+    FOREIGN KEY (city_id) REFERENCES cities (city_id) ON DELETE SET NULL,
     FOREIGN KEY (publishing_houses_id) REFERENCES publishing_houses (publishing_houses_id) ON DELETE SET NULL
 );
 CREATE TABLE IF NOT EXISTS publishing_houses (
     publishing_houses_id uuid PRIMARY KEY,
     name_publishing_houses  VARCHAR(255) NOT NULL,
-    year_of_publishing_houses  INTEGER NOT NULL,
+    year_of_publishing_houses  INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS copies_of_books (
     copies_of_books_id uuid PRIMARY KEY,
